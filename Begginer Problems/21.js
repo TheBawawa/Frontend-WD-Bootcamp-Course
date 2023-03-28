c6bfe9ff51eb2bbf0c4c1a798cba8bbb7f38dcc5
TheBawawa <anagabrielaparedes324@gmail.com>
2023-03-27 22:22:39 -0500

    Beginner Problems

---
+++ Begginer Problems/21.js
@@ -0,0 +1,16 @@
+
+// I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help
+
+function arrayPlusArray(arr1, arr2) {
+    let sum = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        sum1 += arr1[i];
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        sum += arr2[i];
+    }
+
+    return sum; //something went wrong
+}
